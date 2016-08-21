d77c6890d542dcee510460a3714dc28eac63619f
Matthew Scragg <scragg@gmail.com>
2016-08-21 14:35:09 -0400

    example controller

--- index.js
+++ index.js
@@ -5,5 +5,15 @@ router.get('/', function *(next) {
   this.body = 'Stuff';
 });
 
+// Put me in another file if you want
+const ExampleController  = {
+  get: function *(next) {
+    this.body = 'Example Get';
+  }
+}
+
+router.get('/api/example', ExampleController.get);
+
 app.use(router.routes()).use(router.allowedMethods());
+
 app.listen(3000);
